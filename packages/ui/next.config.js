dec3d29fddef7660dfbe66e794c73de627a3b63a
Rosen Stoyanov <me@pockata.org>
2021-04-01 16:05:56 +0300

    build: Add support for directory-named components

---
+++ packages/ui/next.config.js
@@ -0,0 +1,13 @@
+var DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
+
+module.exports = {
+    webpack: (config) => {
+        if (!config.resolve) {
+            config.resolve = {plugins: []};
+        }
+        config.resolve.plugins.push(new DirectoryNamedWebpackPlugin())
+
+        // Important: return the modified config
+        return config
+    },
+}
