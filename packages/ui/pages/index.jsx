33d0c0bfe44225ba67890ea823dd9610f4797d42
Rosen Stoyanov <me@pockata.org>
2021-04-01 16:03:04 +0300

    style: Use .jsx extension

---
+++ packages/ui/pages/index.jsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+// import styles from '../styles/Home.module.css'
+
+import Layout from '../layouts/main';
+
+export default function Home() {
+  return (
+    <Layout>
+        <Head>
+          <title>Въй Лек</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+      leka
+    </Layout>
+  )
+}
