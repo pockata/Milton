3d3c5a8d05a158c17390848cd6a1b89b4d548664
Rosen Stoyanov <me@pockata.org>
2021-04-13 15:11:20 +0300

    feat: Add config file

---
+++ packages/ui/config.example.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'apiURL': 'http://localhost:8080'
+};
+
--- packages/ui/pages/_app.js
+++ packages/ui/pages/_app.js
@@ -2,9 +2,10 @@ import "normalize.css"
 import '../styles/globals.scss'
 
 import { SWRConfig } from 'swr'
+import config from '../config'
 
 function MyApp({ Component, pageProps }) {
-  let fetcher = (resource, init) => fetch(resource, init).then(res => res.json())
+  let fetcher = (resource, init) => fetch(config.apiURL + resource, init).then(res => res.json())
   return (
     <SWRConfig value={{fetcher}} >
       <Component {...pageProps} />
