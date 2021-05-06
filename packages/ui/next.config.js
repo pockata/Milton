f16e8c270a647285e51508c27dead918311a6d4d
Rosen Stoyanov <me@pockata.org>
2021-05-06 15:56:25 +0300

    style: Add next.js types

--- packages/ui/next.config.js
+++ packages/ui/next.config.js
@@ -1,13 +1,14 @@
 var DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
 
 module.exports = {
-    webpack: (config) => {
-        if (!config.resolve) {
-            config.resolve = {plugins: []};
-        }
-        config.resolve.plugins.push(new DirectoryNamedWebpackPlugin())
+	webpack: (config) => {
+		if (!config.resolve) {
+			config.resolve = {plugins: []};
+		}
+		config.resolve.plugins.push(new DirectoryNamedWebpackPlugin());
 
-        // Important: return the modified config
-        return config
-    },
-}
+		// Important: return the modified config
+		return config;
+	},
+};
+
