4ef00b4eaf501645b0f388fd5922adbe98b76231
Rosen Stoyanov <me@pockata.org>
2021-05-12 23:35:37 +0300

    style: Fix linting errors

--- packages/ui/pages/_app.js
+++ packages/ui/pages/_app.js
@@ -1,16 +1,36 @@
-import "normalize.css"
-import '../styles/globals.scss'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import { SWRConfig } from 'swr'
-import config from '../config'
+import "normalize.css";
+import '../styles/globals.scss';
 
+import { SWRConfig } from 'swr';
+import config from '../config';
+
+/**
+ * @param {object} props
+ * @param {React.ComponentType} props.Component
+ * @param {object} props.pageProps
+ *
+ * @return {React.ReactElement}
+ */
 function MyApp({ Component, pageProps }) {
-  let fetcher = (resource, init) => fetch(config.apiURL + resource, init).then(res => res.json())
-  return (
-    <SWRConfig value={{fetcher}} >
-      <Component {...pageProps} />
-    </SWRConfig>
-  )
+	/**
+	 * @param {string} resource
+	 * @param {RequestInit} [init]
+	 **/
+	let fetcher = (resource, init) => fetch(config.apiURL + resource, init).then(res => res.json());
+	return (
+		<SWRConfig value={{fetcher}} >
+			<Component {...pageProps} />
+		</SWRConfig>
+	);
 }
 
-export default MyApp
+MyApp.propTypes = {
+	Component: PropTypes.instanceOf(React.Component),
+	pageProps: PropTypes.object,
+};
+
+export default MyApp;
+
