a07618b02eb23098c2f7c1389447ef6bb16afc51
Rosen Stoyanov <me@pockata.org>
2022-04-12 14:32:33 +0300

    fix(api): Use http method constants

--- packages/server/milton.go
+++ packages/server/milton.go
@@ -30,25 +30,25 @@ func main() {
 
 	w := helpers.CreateAPIWrapHandler(db)
 
-	api.HandleFunc("/query-active-units", w(routes.QueryActiveUnits)).Methods("GET")
+	api.HandleFunc("/query-active-units", w(routes.QueryActiveUnits)).Methods(http.MethodGet)
 
 	// units
-	api.HandleFunc("/get-paired-units", w(routes.GetPairedUnits)).Methods("GET")
-	api.HandleFunc("/pair-unit", w(routes.PairUnit)).Methods("POST")
-	api.HandleFunc("/unpair-unit", w(routes.UnpairUnit)).Methods("POST")
+	api.HandleFunc("/get-paired-units", w(routes.GetPairedUnits)).Methods(http.MethodGet)
+	api.HandleFunc("/pair-unit", w(routes.PairUnit)).Methods(http.MethodPost)
+	api.HandleFunc("/unpair-unit", w(routes.UnpairUnit)).Methods(http.MethodPost)
 
 	// pots
-	api.HandleFunc("/add-pot", w(routes.AddPot)).Methods("POST")
-	api.HandleFunc("/get-pots/{UnitID}", w(routes.GetPots)).Methods("GET")
-	api.HandleFunc("/update-pot", w(routes.UpdatePot)).Methods("POST")
-	api.HandleFunc("/remove-pot", w(routes.RemovePot)).Methods("POST")
+	api.HandleFunc("/add-pot", w(routes.AddPot)).Methods(http.MethodPost)
+	api.HandleFunc("/get-pots/{UnitID}", w(routes.GetPots)).Methods(http.MethodGet)
+	api.HandleFunc("/update-pot", w(routes.UpdatePot)).Methods(http.MethodPost)
+	api.HandleFunc("/remove-pot", w(routes.RemovePot)).Methods(http.MethodPost)
 
 	// watering jobs
-	api.HandleFunc("/add-job", w(routes.AddJob)).Methods("POST")
-	api.HandleFunc("/remove-job", w(routes.RemoveJob)).Methods("POST")
-	api.HandleFunc("/update-job", w(routes.UpdateJob)).Methods("POST")
-	api.HandleFunc("/get-jobs", w(routes.GetJobs)).Methods("GET")
-	api.HandleFunc("/get-job/{JobID}", w(routes.GetJob)).Methods("GET")
+	api.HandleFunc("/add-job", w(routes.AddJob)).Methods(http.MethodPost)
+	api.HandleFunc("/remove-job", w(routes.RemoveJob)).Methods(http.MethodPost)
+	api.HandleFunc("/update-job", w(routes.UpdateJob)).Methods(http.MethodPost)
+	api.HandleFunc("/get-jobs", w(routes.GetJobs)).Methods(http.MethodGet)
+	api.HandleFunc("/get-job/{JobID}", w(routes.GetJob)).Methods(http.MethodGet)
 
 	// TODO: Support PORT env variable
 	// httpPort := os.Getenv("PORT")
